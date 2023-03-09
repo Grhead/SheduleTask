9a8917963dea3993203cc528bf49be54579b23c8
Grenka <64643423+Grhead@users.noreply.github.com>
2023-03-09 19:26:59 +0700

    optimized

--- AuthPack/AuthPack.go
+++ AuthPack/AuthPack.go
@@ -1,7 +1,6 @@
 package AuthPack
 
 import (
-	"fmt"
 	srv "timesheet/Service"
 )
 
@@ -18,8 +17,6 @@ func Authorization(login string, pass string) (bool, int32) {
 	var result IsAuthComplete
 	query := "SELECT Id, Login, Pass, Role FROM Accounts WHERE Login = ? AND Pass = ?"
 	srv.Db.Get(&result, query, login, pass)
-	fmt.Println(result)
-	fmt.Println(result.Id)
 	if result.Id == 0 {
 		IsAccept = false
 		IsAcceptRole = -1
